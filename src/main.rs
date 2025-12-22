96c248b2afa5459da500a4d1cb4b00119b3ce4e7
Tumblewood <20542999+Tumblewood@users.noreply.github.com>
2025-12-21 23:04:45 -0800

    add some early s1 matches

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,6 @@ use ranked_stats::log_reader::MatchIterator;
 use ranked_stats::ranked_ratings::get_ranked_matchups;
 
 fn main() {
-    let match_iterator = MatchIterator::new(394, 415);
+    let match_iterator = MatchIterator::new(394, 417);
     get_ranked_matchups(match_iterator);
 }
